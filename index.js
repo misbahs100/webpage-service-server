7941060ff2f2382ce4f3e49837897a3238a06264
misbahs100 <misbahuddins100@gmail.com>
2021-04-18 10:23:48 +0600

    update service

--- index.js
+++ index.js
@@ -22,125 +22,142 @@ client.connect(err => {
   // create an admin
   app.post('/addAnAdmin', (req, res) => {
     const admin = req.body;
-    console.log("admin: ",admin);
+    console.log("admin: ", admin);
     adminCollection.insertOne(admin)
-        .then(result => {
-          console.log(result.insertedCount)
-            res.send(result.insertedCount > 0);
-        })
-})
-
-// create a service
-app.post('/addService', (req, res) => {
-  const service = req.body;
-  console.log("admin: ",service);
-  serviceCollection.insertOne(service)
       .then(result => {
         console.log(result.insertedCount)
-          res.send(result.insertedCount > 0);
+        res.send(result.insertedCount > 0);
       })
-})
-
- // read all services
- app.get('/services', (req, res) => {
-  serviceCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-})
-
-// read all orders
-app.get('/orders', (req, res) => {
-  ordersCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-})
-
-// read a service
-app.get('/service/:id', (req, res) => {
-  serviceCollection.find({_id: ObjectId(req.params.id)})
-      .toArray((err, documents) => {
-          res.send(documents[0]);
-      })
-})
-
-// create an order
-app.post('/addOrder', (req, res) => {
-  const order = req.body;
-  ordersCollection.insertOne(order)
-      .then(result => {
-        console.log("order: ",result.insertedCount)
-          res.send(result.insertedCount > 0)
-      })
-});
-
- // read a loggedInUser is an admin or not by checking email
- app.post('/isAdmin', (req, res) => {
-  const email = req.body.email;
-  adminCollection.find({ email: email })
-      .toArray((err, admins) => {
-          res.send(admins.length > 0);
-      })
-})
-
- // read some(matched email) orders from database
- app.get('/orders/:email', (req, res) => {
-   console.log(req.params.email);
-  ordersCollection.find({ email: req.params.email })
-    .toArray((err, documents) => {
-      console.log("docss: ", documents)
-      res.send(documents)
-    })
-})
-
-// create a testimonial
-app.post('/addTestimonial', (req, res) => {
-  const testimonial = req.body;
-  console.log("testimonial: ",testimonial);
-  testimonialCollection.insertOne(testimonial)
-      .then(result => {
-        console.log(result.insertedCount)
-          res.send(result.insertedCount > 0);
-      })
-})
-
- // read all testimonials
- app.get('/testimonials', (req, res) => {
-  testimonialCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-})
-
-// update state
-app.post('/updateState/:id', (req, res) => {
-  ordersCollection.updateOne({_id: ObjectId(req.params.id)},
-  {$set: {state: req.body.state}}
-  )
-  .then(result => {
-    console.log(result);
-    res.send(result.modifiedCount > 0);
   })
 
-})
+  // create a service
+  app.post('/addService', (req, res) => {
+    const service = req.body;
+    console.log("admin: ", service);
+    serviceCollection.insertOne(service)
+      .then(result => {
+        console.log(result.insertedCount)
+        res.send(result.insertedCount > 0);
+      })
+  })
 
-// delete one from services collection
-app.delete('/deleteService/:id', (req, res) => {
-  serviceCollection.deleteOne({ _id: ObjectId(req.params.id) })
-    .then(result => {
-      console.log(result);
-      res.send(result.deletedCount > 0);
-    })
-})
+  // read all services
+  app.get('/services', (req, res) => {
+    serviceCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  // read all orders
+  app.get('/orders', (req, res) => {
+    ordersCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  // read a service
+  app.get('/service/:id', (req, res) => {
+    serviceCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  // create an order
+  app.post('/addOrder', (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+      .then(result => {
+        console.log("order: ", result.insertedCount)
+        res.send(result.insertedCount > 0)
+      })
+  });
+
+  // read a loggedInUser is an admin or not by checking email
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+      .toArray((err, admins) => {
+        res.send(admins.length > 0);
+      })
+  })
+
+  // read some(matched email) orders from database
+  app.get('/orders/:email', (req, res) => {
+    console.log(req.params.email);
+    ordersCollection.find({ email: req.params.email })
+      .toArray((err, documents) => {
+        console.log("docss: ", documents)
+        res.send(documents)
+      })
+  })
+
+  // create a testimonial
+  app.post('/addTestimonial', (req, res) => {
+    const testimonial = req.body;
+    console.log("testimonial: ", testimonial);
+    testimonialCollection.insertOne(testimonial)
+      .then(result => {
+        console.log(result.insertedCount)
+        res.send(result.insertedCount > 0);
+      })
+  })
+
+  // read all testimonials
+  app.get('/testimonials', (req, res) => {
+    testimonialCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  // update state
+  app.post('/updateState/:id', (req, res) => {
+    ordersCollection.updateOne({ _id: ObjectId(req.params.id) },
+      { $set: { state: req.body.state } }
+    )
+      .then(result => {
+        console.log(result);
+        res.send(result.modifiedCount > 0);
+      })
+
+  })
+
+  // delete one from services collection
+  app.delete('/deleteService/:id', (req, res) => {
+    serviceCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then(result => {
+        console.log(result);
+        res.send(result.deletedCount > 0);
+      })
+  })
+
+  // update service
+  app.post('/updateService', (req, res) => {
+    console.log(req.body)
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+    const imageURL = req.body.imageURL;
+    serviceCollection.updateOne({ _id: ObjectId(req.body.id) },
+      { $set: { name: name, price: price, description: description, imageURL:  imageURL} }
+    )
+      .then(result => {
+        console.log(result);
+        res.send(result.modifiedCount > 0);
+      })
+
+  })
 
 
 });
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to Webpage Service Database!')
-  })
-  
-  const port = process.env.PORT || 5000;
-  app.listen(port)
+  res.send('Welcome to Webpage Service Database!')
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port)
